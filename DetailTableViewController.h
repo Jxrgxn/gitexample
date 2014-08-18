94f1b9a3d0192ac72b356f66b89860739a5c04e4
KacheMoney <basel.farag@gmail.com>
2014-08-18 15:10:07 -0500

    did nothing

---
+++ DetailTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailTableViewController.h
+//  gitexample
+//
+//  Created by Basel Farag on 8/18/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailTableViewController : UITableViewController
+
+@end
